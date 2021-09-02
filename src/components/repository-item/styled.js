1ed3845860de7d988b7fe37f15a9f54a6f1ee8c8
Leo-Albergaria <leo.albergaria@icloud.com>
2021-09-02 16:15:14 -0300

    first modification

---
+++ src/components/repository-item/styled.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  padding: 8px;
+  border-radius: 8px;
+  border: 3px solid #ccc;
+  margin: 10px 10px;
+  width: 350px;
+  height: 150px;
+  align-content: center;
+  box-shadow:  5px -5px teal; 
+`;
+
+export const WrapperTitle = styled.h2`
+  font-size: 18px;
+  font-weight: bold;
+  margin: 8px 0;
+`;
+
+export const WrapperFullName = styled.h2`
+  font-size: 12px;
+  font-weight: bold;
+  margin: 8px 0;
+  color: white;
+  background-color: rgba(0,0,0,0.5);
+  padding: 5px;
+`;
+
+export const WrapperLink = styled.a`
+  font-size: 16px;
+  font-weight: bold;
+  margin: 8px 0;
+  color: #3182ce;
+`;
